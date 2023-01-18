cce5dc0222bda6f4b5305f652aa8d4a88d23a201
Timo Paasila <timo.paasila@gofore.com>
2023-01-18 13:51:15 +0200

    initial commit

---
+++ src/main/java/fi/vm/sade/eperusteet/eperusteetpdfservice/EperusteetPdfServiceApplication.java
@@ -0,0 +1,13 @@
+package fi.vm.sade.eperusteet.eperusteetpdfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EperusteetPdfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EperusteetPdfServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db.migration/V0_0__perustaso.sql
@@ -0,0 +1,18 @@
+CREATE SEQUENCE hibernate_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+create table dokumentti (
+    id              bigint not null primary key,
+    sisalto_id      bigint,
+    tyyppi          character varying not null,
+    tila            character varying not null,
+    kieli           character varying not null,
+    revision        int4,
+    aloitusaika     timestamp without time zone,
+    valmistumisaika timestamp without time zone,
+    dokumenttidata  oid
+);
