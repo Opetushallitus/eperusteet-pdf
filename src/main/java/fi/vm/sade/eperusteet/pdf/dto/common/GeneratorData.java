b05c104189b64456e01889ee5831d8e2890d8c66
Timo Paasila <timo.paasila@gofore.com>
2023-06-02 15:08:51 +0300

    EP-3763

--- src/main/java/fi/vm/sade/eperusteet/pdf/dto/common/GeneratorData.java
+++ src/main/java/fi/vm/sade/eperusteet/pdf/dto/common/GeneratorData.java
@@ -2,9 +2,12 @@ package fi.vm.sade.eperusteet.pdf.dto.common;
 
 import fi.vm.sade.eperusteet.pdf.dto.enums.DokumenttiTyyppi;
 import fi.vm.sade.eperusteet.pdf.dto.enums.Kieli;
+import fi.vm.sade.eperusteet.pdf.dto.enums.Kuvatyyppi;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -30,5 +33,12 @@ public class GeneratorData {
         generatorData.setKtId(ktId);
         generatorData.setTyyppi(tyyppi);
         return generatorData;
+    }
+
+    public static Map<String, ?> uriParameters(GeneratorData generatorData, Kuvatyyppi kuvatyyppi) {
+        if (generatorData.getKtId() != null) {
+            return Map.of("opsId", generatorData.getId(), "kieli", generatorData.getKieli(), "tyyppi", kuvatyyppi, "ktId", generatorData.getKtId());
+        }
+        return Map.of("opsId", generatorData.getId(), "kieli", generatorData.getKieli(), "tyyppi", kuvatyyppi);
     }
 }
