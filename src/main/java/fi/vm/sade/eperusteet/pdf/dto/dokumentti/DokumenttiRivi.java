c2a81b30e217cb16fe2515176800d281d34c7a27
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-09-27 13:50:36 +0300

    EP-3779-2

--- src/main/java/fi/vm/sade/eperusteet/pdf/dto/dokumentti/DokumenttiRivi.java
+++ src/main/java/fi/vm/sade/eperusteet/pdf/dto/dokumentti/DokumenttiRivi.java
@@ -1,21 +1,26 @@
 package fi.vm.sade.eperusteet.pdf.dto.dokumentti;
 
 import fi.vm.sade.eperusteet.pdf.dto.enums.DokumenttiRiviTyyppi;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DokumenttiRivi {
 
     private ArrayList<String> sarakkeet = new ArrayList<>();
     private Integer colspan = 1;
     private DokumenttiRiviTyyppi tyyppi = DokumenttiRiviTyyppi.DEFAULT;
 
-    public void addSarake(String sarake) {
-        sarakkeet.add(sarake);
+    public void addSarake(String... sarakkeet) {
+        this.sarakkeet.addAll(Arrays.asList(sarakkeet));
     }
 
     public String getBackgroundColor() {
