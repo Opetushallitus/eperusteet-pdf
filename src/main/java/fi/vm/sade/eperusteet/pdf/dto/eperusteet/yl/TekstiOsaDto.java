b067ad835c5cfddaa227597f39e06d588e3ae2d1
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2026-01-19 12:56:23 +0200

    EP-4921-2 (#167)

--- src/main/java/fi/vm/sade/eperusteet/pdf/dto/eperusteet/yl/TekstiOsaDto.java
+++ src/main/java/fi/vm/sade/eperusteet/pdf/dto/eperusteet/yl/TekstiOsaDto.java
@@ -2,12 +2,14 @@ package fi.vm.sade.eperusteet.pdf.dto.eperusteet.yl;
 
 import fi.vm.sade.eperusteet.pdf.dto.common.LokalisoituTekstiDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TekstiOsaDto {
     private Long id;
     private LokalisoituTekstiDto otsikko;
